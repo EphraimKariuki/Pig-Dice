c7a97c21bf6af722f086ea6115fb001e3343c1e1
Ephraim Kariuki <ephraimkariuki1@gmail.com>
2018-07-20 15:01:09 +0300

    Add tion of a Constructor

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// business logic
+function PigDice(players, score, turnTotal){
+  this.players: "players";
+  this.score: "score";
+  this.turnTotal: "turnTotal";
+}
+$(document).ready(function(){
+});
