1ca7814772ef58ae298629a73eec3498e19ffa00
Ephraim Kariuki <ephraimkariuki1@gmail.com>
2018-07-20 15:23:50 +0300

    Business logic for the game

--- js/script.js
+++ js/script.js
@@ -4,5 +4,17 @@ function PigDice(players, score, turnTotal){
   this.score: "score";
   this.turnTotal: "turnTotal";
 }
+function Game(){
+  if (input = "hold") {
+    PigDice.score.push(PigDice.turnTotal);
+  }else if ((input>1) && (input <= 6)){
+    PigDice.turnTotal.push(input);
+    alert("It's still your turn!");
+  } else if (input= 1){
+    PigDice.score.push("0");
+    alert("Oops! Next players turn!");
+  } else if (input < 1){
+    return alert("Enter a number");
+  }
 $(document).ready(function(){
 });
